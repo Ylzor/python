6307b0fc3c32d30f64f732a97cd1b7aae62d6c99
Ylzor <30340384+Ylzor@users.noreply.github.com>
2021-10-05 10:47:36 +0200

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-url = "http://C:/Users/CDA05.AFPA/python/index.html"
+url = "https://github.com/Ylzor/python/blob/main/python/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup)
